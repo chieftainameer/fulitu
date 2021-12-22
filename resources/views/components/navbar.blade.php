6cd984b21dfa6fa37bc450f775e19265cdae4dca
Ameer Hamza <ameer@notwebdesign.com>
2021-12-22 16:32:32 +0100

    add required assets and navbar component to the home page

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-lg">
+    <a href="#" class="navbar-brand" >
+        <img src="{{asset('assets/images/res-logo.png')}}" alt="fulitu logo" height="112" width="112" />
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="mobNavbar">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item px-3 py-2  top-nav">
+                <a href="#" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item px-3 py-2 top-nav">
+                <a href="#" class="nav-link">Menu</a>
+            </li>
+            <li class="nav-item px-3 py-2 top-nav">
+                <a href="#" class="nav-link">Events</a>
+            </li>
+            <li class="nav-item px-3 py-2 top-nav">
+                <a href="#" class="nav-link">Gallery</a>
+            </li>
+            <li class="nav-item px-3 py-2 top-nav">
+                <a href="#" class="nav-link">Contact</a>
+            </li>
+        </ul>
+    </div>
+
+    <button class="btn btn-primary mr-auto book-table-btn">Book A Table</button>
+</nav>
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <!--====== Required meta tags ======-->
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!--====== Title ======-->
+        <title>RESTAURANTE FULITU</title>
+
+        <!--====== Favicon Icon ======-->
+        <link rel="shortcut icon" href="{{asset('assets/images/favicon.png')}}" type="image/png">
+
+        <!--====== Bootstrap css ======-->
+        <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+
+        <!--====== Fontawesome css ======-->
+        <link rel="stylesheet" href="{{asset('assets/css/font-awesome.min.css')}}">
+
+        <!--====== Line Icons css ======-->
+        <link rel="stylesheet" href="{{asset('assets/css/LineIcons.css')}}">
+
+        <!--====== Animate css ======-->
+        <link rel="stylesheet" href="{{ asset('assets/css/animate.css') }}">
+
+        <!--====== Aos css ======-->
+        <link rel="stylesheet" href="{{asset('assets/css/aos.css')}}">
+
+        <!--====== Slick css ======-->
+        <link rel="stylesheet" href="{{asset('assets/css/slick.css')}}">
+
+        <!--====== Default css ======-->
+        <link rel="stylesheet" href="{{asset('assets/css/default.css')}}">
+
+        <!--====== Style css ======-->
+        <link rel="stylesheet" href="{{asset('assets/css/custom.css')}}">
+    </head>
+    <body class="antialiased">
+    <x-navbar />
+    <x-header />
+
+    <!--====== jquery js ======-->
+    <script src="{{ asset('assets/js/vendor/modernizr-3.6.0.min.js') }}"></script>
+    <script src="{{asset('assets/js/vendor/jquery-1.12.4.min.js')}}"></script>
+
+    <!--====== Bootstrap js ======-->
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+
+    <!--====== WOW js ======-->
+    <script src="{{ asset('assets/js/wow.min.js') }}"></script>
+
+    <!--====== Slick js ======-->
+    <script src="{{ asset('assets/js/slick.min.js') }}"></script>
+
+    <!--====== Scrolling Nav js ======-->
+    <script src="{{ asset('assets/js/scrolling-nav.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.easing.min.js') }}"></script>
+
+    <!--====== Aos js ======-->
+    <script src="{{ asset('assets/js/aos.js') }}"></script>
+
+    <!--====== Main js ======-->
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+    </body>
+</html>
